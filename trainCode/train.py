404678289653d195b2888f3e91e4e714b19f4064
Kai Chen Chai <englishorc65@gmail.com>
2024-09-02 20:35:24 +0800

    issues training model from scratch, val errs

--- trainCode/train.py
+++ trainCode/train.py
@@ -3,10 +3,10 @@ from wandb.integration.ultralytics import add_wandb_callback
 import wandb
 
 # Step 1: Initialize a Weights & Biases run
-wandb.init(project="VinDr_YOLOv8", job_type="training", name = "200824_YOLOv8s_brightnessEQ_FIXED_FIXEDBB",
+wandb.init(project="VinDr_YOLOv8", job_type="training", name = "02092024_YOLOv8s_no_pretrained",
 config={
     "epochs": 50,
-    "dataset": "FULL_brightnessEQ_VinDr_FIXED",
+    "dataset": "FULL_brightnessEQ_VinDr_FIXED_FIXEDBB",
     "model": "YOLOv8s",
     "image_size": 1024,
     "batch_size": 16,
@@ -16,7 +16,7 @@ config={
 # Step 2: Define the YOLOv8 Model and Dataset
 model_name = "yolov8s"
 dataset_name = "model.yaml"
-model = YOLO(f"{model_name}.pt")
+model = YOLO(f"{model_name}.yaml")
 
 # Step 3: Add W&B Callback for Ultralytics
 add_wandb_callback(model, enable_model_checkpointing=True)
@@ -24,9 +24,10 @@ add_wandb_callback(model, enable_model_checkpointing=True)
 # Step 4: Train and Fine-Tune the Model
 model.train(project = "train_VinDr_YOLOv8",
             data = dataset_name,
-            name = "200824_YOLOv8s_brightnessEQ_FIXED_FIXEDBB",
+            name = "02092024_YOLOv8s_no_pretrained",
             batch = 16,
-            epochs = 50,
+            epochs = 2,
+            pretrained = False,
             imgsz = 1024,
             plots = True,
             device=[0])
@@ -35,7 +36,7 @@ model.train(project = "train_VinDr_YOLOv8",
 try:
   model.val(data = dataset_name)
 except AssertionError as e:
-  print(f"Error Excepted: {e}")
+  print(AssertionError)
 
 # Step 7: Finalize the W&B Run
 wandb.finish()
