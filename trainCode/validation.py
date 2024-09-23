faf945543af687c0dcd8092dbe0f0443d671b9f6
Kai Chen Chai <englishorc65@gmail.com>
2024-09-23 14:39:59 +0800

    Testing with 0.1 threshold

--- trainCode/validation.py
+++ trainCode/validation.py
@@ -3,14 +3,14 @@ from wandb.integration.ultralytics import add_wandb_callback
 import wandb
 
 # Step 1: Initialize a Weights & Biases run
-wandb.init(project="train_VinDr_YOLOv8", job_type="validation", name = "19092024_YOLOv8m_subset-C-merged_VAL_1DET",
+wandb.init(project="train_VinDr_YOLOv8", job_type="validation", name = "23092024_YOLOv8m_subset-C-merged_VAL_1DET",
 config={
     "dataset": "FULL_brightnessEQ_VinDr_FIXED",
     "model": "YOLOv8m_best.pt",
     "image_size": 1024,
     "batch": 16,
     "machine": "Thermaltake_2080ti_0",
-    "conf": 0
+    "conf": 0.01
 }
 )
 best = "/mnt/data/kai/VinDr_YOLOv8_experiments/trainCode/train_VinDr_YOLOv8/12092024_YOLOv8m_subset-C-merged/weights/best.pt"
@@ -25,7 +25,7 @@ add_wandb_callback(model, enable_model_checkpointing=True)
 #validation info
 metrics = model.val(project = "train_VinDr_YOLOv8",
             data = dataset_name,
-            name = "19092024_YOLOv8m_subset-C-merged_VAL1DET",
+            name = "23092024_YOLOv8m_subset-C-merged_VAL1DET",
             imgsz = 1024,
             plots = True,
             batch = 16,
@@ -33,7 +33,7 @@ metrics = model.val(project = "train_VinDr_YOLOv8",
             device=[0,1],
             split = "test",
             max_det = 1,
-            conf = 0)
+            conf = 0.01)
 
 
 
