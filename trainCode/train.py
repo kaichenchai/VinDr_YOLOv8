beb9fa4d606e7991881e26b3d2da18008bcb7525
kaichenchai <englishorc65@gmail.com>
2025-05-11 19:51:16 +0800

    working on training

--- trainCode/train.py
+++ trainCode/train.py
@@ -2,21 +2,11 @@ from ultralytics import YOLO
 from wandb.integration.ultralytics import add_wandb_callback
 import wandb
 
-# Step 1: Initialize a Weights & Biases run
-wandb.init(project="VinDr_YOLOv8", job_type="training", name = "130824_YOLOv8s_brightnessEQ_FIXED",
-config={
-    "epochs": 50,
-    "dataset": "FULL_brightnessEQ_VinDr_FIXED",
-    "model": "YOLOv8s",
-    "image_size": 1024,
-    "batch_size": 16,
-    "machine": "Thermaltake_2080ti_0"
-}
-)
+wandb.require("core")
+
 # Step 2: Define the YOLOv8 Model and Dataset
-model_name = "yolov8s"
 dataset_name = "model.yaml"
-model = YOLO(f"{model_name}.pt")
+model = YOLO("yolov8m.pt")
 
 # Step 3: Add W&B Callback for Ultralytics
 add_wandb_callback(model, enable_model_checkpointing=True)
@@ -24,18 +14,11 @@ add_wandb_callback(model, enable_model_checkpointing=True)
 # Step 4: Train and Fine-Tune the Model
 model.train(project = "train_VinDr_YOLOv8",
             data = dataset_name,
-            name = "130824_YOLOv8s_brightnessEQ_FIXED",
+            name = "24042025_YOLOv8m_FULL_1024_CLAHE_VinDr_padding",
             batch = 16,
             epochs = 50,
             imgsz = 1024,
             plots = True,
             device=[0])
 
-# Step 5: Validate the Model
-try:
-  model.val()
-except AssertionError as e:
-  print(f"Error Excepted: {e}")
-
-# Step 7: Finalize the W&B Run
 wandb.finish()
