225d4188a6b10e5b084f660e22fbbfd868efa4d4
kaichenchai <englishorc65@gmail.com>
2025-05-12 00:48:30 +0800

    training clahe cardiomegaly model

--- trainCode/train.py
+++ trainCode/train.py
@@ -17,12 +17,12 @@ add_wandb_callback(model, enable_model_checkpointing=True)
 # Step 4: Train and Fine-Tune the Model
 model.train(project = "train_VinDr_YOLOv8",
             data = dataset_name,
-            name = "12092024_YOLOv8m_subset-C-merged",
+            name = "110525_YOLOv8m_subset-C-merged-CLAHE",
             epochs = 50,
             batch = 16,
             imgsz = 1024,
             plots = True,
-            device=[0, 1]
+            device=[0]
             )
 
 wandb.finish()
