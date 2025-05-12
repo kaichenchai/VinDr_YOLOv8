7eaa18ebb757a30de2dd7559096b6b8141587617
kaichenchai <englishorc65@gmail.com>
2025-05-12 22:37:56 +0800

    trained model, ran validation

--- trainCode/train.py
+++ trainCode/train.py
@@ -17,12 +17,14 @@ add_wandb_callback(model, enable_model_checkpointing=True)
 # Step 4: Train and Fine-Tune the Model
 model.train(project = "train_VinDr_YOLOv8",
             data = dataset_name,
-            name = "110525_YOLOv8m_subset-C-merged-CLAHE",
+            name = "120525_YOLOv8m_subset-C-merged-CLAHE_512",
             epochs = 50,
             batch = 16,
-            imgsz = 1024,
+            imgsz = 512,
             plots = True,
-            device=[0]
+            device=[0],
+            optimizer="Adam",
+            save_period=10
             )
 
 wandb.finish()
--- trainCode/validation.py
+++ trainCode/validation.py
@@ -3,17 +3,17 @@ from wandb.integration.ultralytics import add_wandb_callback
 import wandb
 
 # Step 1: Initialize a Weights & Biases run
-wandb.init(project="train_VinDr_YOLOv8", job_type="validation", name = "23092024_YOLOv8m_subset-C-merged_VAL_1DET",
+wandb.init(project="train_VinDr_YOLOv8", job_type="validation", name = "120525_YOLOv8m_subset-C-merged-CLAHE_512_VAL",
 config={
-    "dataset": "FULL_brightnessEQ_VinDr_FIXED",
+    "dataset": "FULL_1024_padding_CLAHE",
     "model": "YOLOv8m_best.pt",
-    "image_size": 1024,
+    "image_size": 512,
     "batch": 16,
     "machine": "Thermaltake_2080ti_0",
-    "conf": 0.01
+    "conf": 0.10
 }
 )
-best = "/mnt/data/kai/VinDr_YOLOv8_experiments/trainCode/train_VinDr_YOLOv8/12092024_YOLOv8m_subset-C-merged/weights/best.pt"
+best = "/mnt/data/kai/VinDr_Code/VinDr_YOLOv8/trainCode/train_VinDr_YOLOv8/120525_YOLOv8m_subset-C-merged-CLAHE_512/weights/best.pt"
 dataset_name = "subset.yaml"
 
 #load the best of the trained model
@@ -25,15 +25,15 @@ add_wandb_callback(model, enable_model_checkpointing=True)
 #validation info
 metrics = model.val(project = "train_VinDr_YOLOv8",
             data = dataset_name,
-            name = "23092024_YOLOv8m_subset-C-merged_VAL1DET",
-            imgsz = 1024,
+            name = "120525_YOLOv8m_subset-C-merged-CLAHE_512_VAL",
+            imgsz = 512,
             plots = True,
             batch = 16,
             save_json = True,
-            device=[0,1],
+            device=[0],
             split = "test",
             max_det = 1,
-            conf = 0.01)
+            conf = 0.10)
 
 
 
