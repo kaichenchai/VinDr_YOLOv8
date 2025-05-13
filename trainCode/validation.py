71f1720c6eadf7eb4e1ba5ede027a3261c9852eb
kaichenchai <englishorc65@gmail.com>
2025-05-13 10:44:51 +0800

    working with last.pt

--- trainCode/validation.py
+++ trainCode/validation.py
@@ -3,17 +3,17 @@ from wandb.integration.ultralytics import add_wandb_callback
 import wandb
 
 # Step 1: Initialize a Weights & Biases run
-wandb.init(project="train_VinDr_YOLOv8", job_type="validation", name = "120525_YOLOv8m_subset-C-merged-CLAHE_512_VAL",
+wandb.init(project="train_VinDr_YOLOv8", job_type="validation", name = "130525_YOLOv8m_subset-C-merged-CLAHE_512_VAL",
 config={
     "dataset": "FULL_1024_padding_CLAHE",
-    "model": "YOLOv8m_best.pt",
+    "model": "YOLOv8m_last.pt",
     "image_size": 512,
     "batch": 16,
     "machine": "Thermaltake_2080ti_0",
     "conf": 0.10
 }
 )
-best = "/mnt/data/kai/VinDr_Code/VinDr_YOLOv8/trainCode/train_VinDr_YOLOv8/120525_YOLOv8m_subset-C-merged-CLAHE_512/weights/best.pt"
+best = "/mnt/data/kai/VinDr_Code/VinDr_YOLOv8/trainCode/train_VinDr_YOLOv8/120525_YOLOv8m_subset-C-merged-CLAHE_512/weights/last.pt"
 dataset_name = "subset.yaml"
 
 #load the best of the trained model
@@ -25,7 +25,7 @@ add_wandb_callback(model, enable_model_checkpointing=True)
 #validation info
 metrics = model.val(project = "train_VinDr_YOLOv8",
             data = dataset_name,
-            name = "120525_YOLOv8m_subset-C-merged-CLAHE_512_VAL",
+            name = "130525_YOLOv8m_subset-C-merged-CLAHE_512_VAL",
             imgsz = 512,
             plots = True,
             batch = 16,
