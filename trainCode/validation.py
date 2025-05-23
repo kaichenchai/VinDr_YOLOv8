ebfc23898eeca073bd982add1796102fe2e4e596
kaichenchai <englishorc65@gmail.com>
2025-05-23 10:17:23 +0800

    validation with model 0.1 and 0.2 cutoffs

--- trainCode/validation.py
+++ trainCode/validation.py
@@ -3,17 +3,17 @@ from wandb.integration.ultralytics import add_wandb_callback
 import wandb
 
 # Step 1: Initialize a Weights & Biases run
-wandb.init(project="train_VinDr_YOLOv8", job_type="validation", name = "130525_YOLOv8m_subset-C-merged-CLAHE-bone-suppression_512_VAL",
+wandb.init(project="train_VinDr_YOLOv8", job_type="validation", name = "230525_YOLOv8m_subset-C-merged-CLAHE-bone-suppression-pretrained-ssl_VAL",
 config={
-    "dataset": "FULL_1024_padding_CLAHE",
+    "dataset": "FULL_1024_padding_CLAHE_bone_suppression",
     "model": "YOLOv8m_last.pt",
     "image_size": 512,
     "batch": 16,
     "machine": "RTX4090",
-    "conf": 0.10
+    "conf": 0.20
 }
 )
-best = "/mnt/data/kai/VinDr_Code/VinDr_YOLOv8/trainCode/train_VinDr_YOLOv8/120525_YOLOv8m_subset-C-merged-CLAHE-bone-suppression2/weights/last.pt"
+best = "/mnt/data/kai/VinDr_Code/VinDr_YOLOv8/trainCode/train_VinDr_YOLOv8/220525_YOLOv8m_subset-C-merged-CLAHE-bone-suppression-pretrained-ssl5/weights/last.pt"
 dataset_name = "subset.yaml"
 
 #load the best of the trained model
@@ -25,7 +25,7 @@ add_wandb_callback(model, enable_model_checkpointing=True)
 #validation info
 metrics = model.val(project = "train_VinDr_YOLOv8",
             data = dataset_name,
-            name = "130525_YOLOv8m_subset-C-merged-CLAHE-bone-suppression_512_VAL",
+            name = "230525_YOLOv8m_subset-C-merged-CLAHE-bone-suppression-pretrained-ssl_VAL",
             imgsz = 512,
             plots = True,
             batch = 16,
@@ -33,7 +33,7 @@ metrics = model.val(project = "train_VinDr_YOLOv8",
             device=[0],
             split = "test",
             max_det = 1,
-            conf = 0.10)
+            conf = 0.20)
 
 
 
