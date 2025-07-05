9b0acae4a3778ebcad5e458da51cc5971b947fbb
kaichenchai <englishorc65@gmail.com>
2025-07-05 17:23:06 +0800

    training yolov11s model

--- trainCode/train.py
+++ trainCode/train.py
@@ -3,12 +3,12 @@ from ultralytics import YOLO, settings
 settings.update({"wandb":True})
 
 dataset = "data.yaml"
-model = YOLO(f"yolo11l.pt")
+model = YOLO(f"yolo11s.pt")
 
 model.train(project = "cardiomegaly_explainability",
             data = dataset,
-            name = "05072025_yolov11l_histEQ",
-            batch = 8,
+            name = "05072025_yolov11s_histEQ",
+            batch = 16,
             epochs = 500,
             imgsz = 1024,
             plots = True,
