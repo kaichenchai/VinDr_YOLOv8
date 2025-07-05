4b4c1642819d9c32eddc0a1d71d35841a553fb3f
kaichenchai <englishorc65@gmail.com>
2025-07-05 12:47:42 +0800

    updates to train code

--- trainCode/train.py
+++ trainCode/train.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 from wandb.integration.ultralytics import add_wandb_callback
 import wandb
 
-wandb.init(project="train_VinDr_YOLOv8", job_type="training", name = "220525_YOLOv8m_subset-C-merged-CLAHE-bone-suppression",
+wandb.init(project="train_VinDr_YOLOv8", job_type="training", name = "220525_YOLOv8m_subset-C-merged-CLAHE-bone-suppression-no-pretrained",
 config={
     "epochs": 100,
     "dataset": "FULL_1024_CLAHE_padding",
@@ -27,8 +27,9 @@ add_wandb_callback(model, enable_model_checkpointing=True)
 # Step 4: Train and Fine-Tune the Model
 model.train(project = "train_VinDr_YOLOv8",
             data = dataset_name,
-            name = "230525_YOLOv8m_subset-C-merged-CLAHE-bone-suppression",
+            name = "230525_YOLOv8m_subset-C-merged-CLAHE-bone-suppression-no-pretrained",
             epochs = 100,
+            pretrained=False,
             batch = 16,
             imgsz = 512,
             plots = True,
