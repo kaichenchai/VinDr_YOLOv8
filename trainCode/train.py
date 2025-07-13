50a461790d08fcb1966337a0d6de434d45bab93c
kaichenchai <englishorc65@gmail.com>
2025-07-13 22:22:08 +0800

    training YOLOv11m with CLAHE

--- trainCode/train.py
+++ trainCode/train.py
@@ -3,11 +3,11 @@ from ultralytics import YOLO, settings
 settings.update({"wandb":True})
 
 dataset = "data.yaml"
-model = YOLO(f"yolo11n.pt")
+model = YOLO(f"yolo11m.pt")
 
 model.train(project = "cardiomegaly_explainability",
             data = dataset,
-            name = "05072025_yolov11n_histEQ",
+            name = "06072025_yolov11m_CLAHE",
             batch = 16,
             epochs = 500,
             imgsz = 1024,
