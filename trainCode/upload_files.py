85f23623757cd9c1fbd4f7b7cdc6320f650ebed5
kaichenchai <englishorc65@gmail.com>
2025-07-13 22:26:02 +0800

    uploading weights

--- trainCode/upload_files.py
+++ trainCode/upload_files.py
@@ -1,7 +1,7 @@
 import wandb
 
 if __name__ == "__main__":
-    run = wandb.init(project="cardiomegaly_explainability", id="i1j0eczu")
-    wandb.save(glob_str="/home/kai/mnt/VinDr_Code/VinDr_YOLOv8/trainCode/cardiomegaly_explainability/05072025_yolov11n_histEQ/weights/*",
-            base_path="/home/kai/mnt/VinDr_Code/VinDr_YOLOv8/trainCode/cardiomegaly_explainability/05072025_yolov11n_histEQ/")
+    run = wandb.init(project="cardiomegaly_explainability", id="fj00w39p")
+    wandb.save(glob_str="/home/kai/mnt/VinDr_Code/VinDr_YOLOv8/trainCode/cardiomegaly_explainability/06072025_yolov11m_CLAHE2/weights/*",
+            base_path="/home/kai/mnt/VinDr_Code/VinDr_YOLOv8/trainCode/cardiomegaly_explainability/06072025_yolov11m_CLAHE2/")
     run.finish()
