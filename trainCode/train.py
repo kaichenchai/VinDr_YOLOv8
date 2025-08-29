e50350bdc724e2175863c2fd494fbf989c0acd7e
kaichenchai <englishorc65@gmail.com>
2025-08-29 10:09:46 +0800

    training axis oriented bb

--- trainCode/train.py
+++ trainCode/train.py
@@ -3,12 +3,12 @@ from ultralytics import YOLO, settings
 settings.update({"wandb":True})
 
 dataset = "data.yaml"
-model = YOLO(f"yolo11m-obb.pt")
+model = YOLO("yolo11m.pt")
 
-model.train(project = "pneumothorax-obb",
+model.train(project = "pneumothorax",
             data = dataset,
-            name = "210825_yolov11m_512",
-            batch = 32,
+            name = "210825_yolov11m_512_bb",
+            batch = 64,
             epochs = 500,
             imgsz = 512,
             plots = True,
