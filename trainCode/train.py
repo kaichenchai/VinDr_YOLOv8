25566d96655678f3adf72bb2428aaef058964a7f
kaichenchai <englishorc65@gmail.com>
2025-08-29 13:51:13 +0800

    trained model with CLAHE

--- trainCode/train.py
+++ trainCode/train.py
@@ -3,17 +3,17 @@ from ultralytics import YOLO, settings
 settings.update({"wandb":True})
 
 dataset = "data.yaml"
-model = YOLO(f"yolo11m-obb.pt")
+model = YOLO("yolo11m-obb.pt")
 
-model.train(project = "pneumothorax-obb",
+model.train(project = "pneumothorax",
             data = dataset,
-            name = "210825_yolov11m_512",
-            batch = 32,
-            epochs = 500,
+            name = "290825_yolov11m_512_CLAHE_obb",
+            batch = 64,
+            epochs = 200,
             imgsz = 512,
             plots = True,
             device=[0],
             optimizer="Adam",
-            patience=100,
+            patience=25,
             seed=12048)
 
