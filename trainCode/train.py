35a63ee6f91a1c076c99aca6deb8fce32849f773
kaichenchai <englishorc65@gmail.com>
2025-09-04 23:31:36 +0800

    trained CLAHE model

--- trainCode/train.py
+++ trainCode/train.py
@@ -7,13 +7,13 @@ model = YOLO("yolo11m-obb.pt")
 
 model.train(project = "pneumothorax",
             data = dataset,
-            name = "290825_yolov11m_512_CLAHE_obb",
+            name = "020925_yolov11m_512_CLAHE_obb",
             batch = 64,
             epochs = 200,
             imgsz = 512,
             plots = True,
             device=[0],
             optimizer="Adam",
-            patience=25,
+            patience=50,
             seed=12048)
 
